7d607701fab072ec9799ecaf2f13ef86d20859b5
Sheldrick <mrmayrant@yahoo.com>
2018-10-07 21:14:17 -0400

    added the POST request method

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const http = new easyHTTP();
 
 // Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
+// http.get("https://jsonplaceholder.typicode.com/posts", function(err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create data
+const data = {
+    title: 'Custom post',
+    body: 'This is a custom post'
+}
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post)
+    }
 });
--- easyHttp.js
+++ easyHttp.js
@@ -1,30 +1,37 @@
 function easyHTTP() {
-  this.http = new XMLHttpRequest();
+    this.http = new XMLHttpRequest();
 }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url, callback) {
-  this.http.open("GET", url, true);
+easyHTTP.prototype.get = function (url, callback) {
+    this.http.open("GET", url, true);
 
-  let self = this;
-  this.http.onload = function() {
-    //   if status is 'ok' pass the response as an argument in the callback
-    if (self.http.status === 200) {
-      callback(null, self.http.responseText);
-      //   otherwise, return the status of the error
-    } else {
-      callback("Error: " + self.http.status);
-    }
-  };
+    let self = this;
+    this.http.onload = function () {
+        //   if status is 'ok' pass the response as an argument in the callback
+        if (self.http.status === 200) {
+            callback(null, self.http.responseText);
+            //   otherwise, return the status of the error
+        } else {
+            callback("Error: " + self.http.status);
+        }
+    };
 
-  this.http.send();
+    this.http.send();
 };
 
 // Make an HTTP POST Request
-easyHTTP.prototype.post = function(url, data, callback) {
-  this.http.open("POST", url, true);
+easyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open("POST", url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
 
-  this.send(JSON.stringify(data));
+    let self = this;
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    };
+
+    //converts data to JSON string
+    this.http.send(JSON.stringify(data));
 };
 // Make an HTTP PUT Request
 
